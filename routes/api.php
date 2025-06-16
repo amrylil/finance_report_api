5f7c7bb43b0daf1c7bd1d549f8b5e6af5ab3b180
Ulil Amry <ulilamry432@gmail.com>
2025-06-17 03:53:16 +0800

    saya frustasi

--- routes/api.php
+++ routes/api.php
@@ -12,13 +12,6 @@ Route::post('/register', [AuthController::class, 'register']);
 
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('/', function () {
-  return response()->json([
-    'status'  => 'sukses',
-    'message' => 'Koneksi ke aplikasi Laravel berhasil! Database tidak dites.'
-  ]);
-});
-
 // ===================================================================
 // RUTE TERPROTEKSI (Wajib Login & Mengirimkan Token Sanctum)
 // ===================================================================
